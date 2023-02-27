586cfca994a6c1f0e0fc431771fae2a9c58293ed
Ellinor Nilsson Kaufeldt <Ellinor.NilssonKaufeldt@sj.se>
2023-02-27 22:37:35 +0100

    Moved app file and added missing props. Very sneaky for the volvo team to remove this so that the build in routing didn't work

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from "next/app";
+import React from "react";
+import { StyleProvider, ThemePicker } from "vcc-ui";
+
+function App({ Component, pageProps }: AppProps) {
+
+  return (
+    <React.StrictMode>
+      <StyleProvider>
+        <ThemePicker variant="light">
+          <Component {...pageProps} />
+        </ThemePicker>
+      </StyleProvider>
+    </React.StrictMode>
+  );
+}
+
+export default App;
